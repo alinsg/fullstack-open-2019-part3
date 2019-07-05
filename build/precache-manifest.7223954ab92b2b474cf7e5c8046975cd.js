19005af0a96f9d4475de5b95259f44d556e2afbd
Alin Simion <simion.g.alin@gmail.com>
2019-07-05 09:58:41 +0300

    Added build files from frontend project

---
+++ build/precache-manifest.7223954ab92b2b474cf7e5c8046975cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b5e147006f90925fcc0e3d47c5d40c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53d4d9a1b432606af141",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "53d4d9a1b432606af141",
+    "url": "/static/js/main.33c5bd81.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
